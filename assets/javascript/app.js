e897077e95cd538fd47e4ccefde6d126326b7a11
Karns <pkarns@deloitte.com>
2017-03-06 21:26:33 -0500

    added moment.js to get train time difference and remainder

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,15 +18,20 @@ $("#add-train-btn").on("click", function () {
 //storing the values entered in text boxes to variables
 	var leavingFrom = $("#train-name-input").val().trim();
 	var destination = $("#destination-input").val().trim();
-	var firstTrain = moment($("#first-train-input").val().trim(), "HH:mm").format("X");
-	var frequency = moment($("#frequency-input").val().trim(), "mm").format("X");
-
+	var firstTrain = moment($("#first-train-input").val().trim(), "HH:mm").format("HH:mm");
+	var frequency = moment($("#frequency-input").val().trim(), "mm").format("mm");
+	var currentTime = moment();
+	var difference = moment.utc(moment(currentTime, "HH:mm").diff(moment(firstTrain, "HH:mm"))).format("HH:mm");
+	var remainder = difference % frequency;
+	console.log(remainder);
+	
 //creating local object to store values in
 	var newTrain = {
 		leaving: leavingFrom,
 		destination: destination,
 		firstTrain: firstTrain,
-		frequency: frequency
+		frequency: frequency,
+		difference: difference
 	};
 
 //upload the train data to firebase
@@ -61,4 +66,47 @@ database.ref().on("child_added", function(childSnapshot) {
 	console.log(destination);
 	console.log(firstTrain);
 	console.log(frequency);
+
+	var currentTime = moment();
+	var difference = moment.utc(moment(currentTime, "HH:mm").diff(moment(firstTrain, "HH:mm"))).format("HH:mm");
+	console.log(difference);
+	var difference = childSnapshot.val().difference;
+	console.log(difference);
+	var toMinutes = moment.duration(difference).asMinutes();
+	console.log(toMinutes);
+	var remainder = toMinutes % frequency;
+	console.log(remainder);
+	// var difference = moment.duration().minutes();
+	// console.log(difference);
+	// 
+	// console.log(remainder);
+	// var thisTime = moment.utc(currentTime, "HH:mm");
+	// var beginTime = moment.utc(firstTrain, "HH:mm");
+
+	// if (beginTime.isAfter(thisTime)) thisTime.add(1, "day");
+
+	// var duration = moment.duration(beginTime.diff(thisTime));
+	// var difference = moment.utc(+duration).format("HH:mm");
+	// console.log(difference);
+
+	// var currentTime = moment();
+	// console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
+	// console.log(currentTime);
+
+	// var betweenTime = (moment.(currentTime.format("HH:mm"))) - (moment.(firstTrain.format("HH:mm")));
+	// console.log("Difference" + moment.(betweenTime).format("HH:mm"));
+
+	//var firstTrainConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
+	
+
+	// var nextArrival = 1645;
+	// var minutesAway = 5;
+
+	// //var nextArrival = first train time + frequency(min) + currentTime;
+	// //var currentTime-lastArrival = minutesAway;
+
+	// //add each train's data to the table
+	// $("#train-table > tbody").append("<tr><td>" + leavingFrom + "</td><td>"	+ destination + "</td><td>"	
+	// 	+ frequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>"); 
+
 });
